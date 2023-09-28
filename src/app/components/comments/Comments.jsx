05091559a3aaa8d156b3b9b0160842a7f8d153e7
Can Surer <cansurer@outlook.com>
2023-09-28 20:36:06 +0200

    finish comment section

--- src/app/components/comments/Comments.jsx
+++ src/app/components/comments/Comments.jsx
@@ -1,78 +1,86 @@
+"use client";
+
 import styles from "./comments.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-const Comments = () => {
-  const status = "authenticated";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+  return data;
+};
+
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+
+  const { data, mutate, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+    setDesc(""); 
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea className={styles.input} placeholder="write a comment..." />
-          <button className={styles.send}>Send</button>
+          <textarea
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+            placeholder="write a comment..."
+            value={desc}
+          />
+          <button onClick={handleSubmit} className={styles.send}>
+            Send
+          </button>
         </div>
       ) : (
         <Link href="/login">Login to write a comment</Link>
       )}
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              width={50}
-              height={50}
-              alt=""
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>01.01.2023</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem morem xxxxxxxxxxxxxxxx xxxxxx dolar sit amet lorem ipsum dolar
-            sit amet lorem ipsum dolar sit amet kireujm iosun dioker sut anmet
-          </p>
-        </div>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              width={50}
-              height={50}
-              alt=""
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>01.01.2023</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem morem xxxxxxxxxxxxxxxx xxxxxx dolar sit amet lorem ipsum dolar
-            sit amet lorem ipsum dolar sit amet kireujm iosun dioker sut anmet
-          </p>
-        </div>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              width={50}
-              height={50}
-              alt=""
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>01.01.2023</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem morem xxxxxxxxxxxxxxxx xxxxxx dolar sit amet lorem ipsum dolar
-            sit amet lorem ipsum dolar sit amet kireujm iosun dioker sut anmet
-          </p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item.user.image ? (
+                    <Image
+                      src={item?.user?.image}
+                      width={50}
+                      height={50}
+                      alt=""
+                      className={styles.image}
+                    />
+                  ) : (
+                    ""
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item?.user?.name}</span>
+                    <span className={styles.date}>01.01.2023</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item?.desc}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
